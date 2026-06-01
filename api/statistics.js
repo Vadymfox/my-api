ad1607dbdbea9c3483daabe90278da7ccf85c3c4
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 09:40:47 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -2,11 +2,10 @@ export default async function handler(req, res) {
   try {
     const targetUrl = "https://адрес-сайта-организации";
 
-    // Передаём cookie в заголовке
     const response = await fetch(targetUrl, {
       headers: {
         "Cookie": "cookieyes-consent=consentid:dDVsVFdQZW5vYTV2TXBLYzhXQ3R3QU10cHYyeXNGTWk,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no; csrftoken=J2t6jjRNZbA0BSAANwFshop1V3Po1fF2; access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NDI5IiwidXNlciI6Ikxpc2l0c3luLlYiLCJleHAiOjE3ODAyOTg1NzcsImlhdCI6MTc4MDI5NDk3NywianRpIjoiNjcwMDk5In0.z482efurc-EL17SOXuiele2I9ZmpFL66ykprx-OSzpjU6NGtcHlplGM4inLTMChYPKk5Z2oy4NDdU0OStl98JrwJBRmgYJ7QwT7qd_4wVqOGit0SybbY7rlfroWLnNCGwPecK1BMTjXBdyFOBasZDnQo8_WAXoWBJVc0D_FNsxjDDzplTc_ZQC3y9Sgq4X5eRLdK9fUb_m2rg9HwS3U2LZLUJC_Q0bGNoXOHIut9b86aJxr7RbqunjMleRBl48lz_vdi9dp3zaaoVgvclqa_gy6dL1wpHJZuwI7bgoeqaqNBQTzSI9xOwGmgQUUIwm9MDzE6M7HAnU3_Zi_qA-HVtw; access_token_exists=true",
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
       }
     });
 
@@ -17,8 +16,8 @@ export default async function handler(req, res) {
     }
 
     const html = await response.text();
-
     const match = html.match(/<table[\s\S]*?<\/table>/i);
+
     if (!match) {
       return res.status(404).json({ error: "Таблица не найдена" });
     }
