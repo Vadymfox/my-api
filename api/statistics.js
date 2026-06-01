1daac26007f5a4951a61da3eca85e82afc4b3d53
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 13:29:07 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -29,7 +29,8 @@ export default async function handler(req, res) {
       headers: {
         fetch("https://main.prod.m11g.ajax.systems/statistics/api/v3/by_grouping_name/?stage=assembling&format=region&location=is76&response_tz=Europe%2FKyiv&time_from=2026-05-30T08%3A00%3A00%2B03%3A00&time_to=2026-05-31T20%3A00%3A00%2B03%3A00&grouping_name0=product_name&grouping_name2=full_name&show_unique=true&unique_by=ftt&register_types=dev&register_types=central&register_types=component&register_types=accessory", {
   "headers": {
-    "accept": "application/json, text/plain, */*",
+  */
+   // "accept": "application/json, text/plain, */*",
 /*    "accept-language": "uk,en-US;q=0.9,en;q=0.8,ru;q=0.7",
     "authorization": "Token undefined",
     "requestuuid": "de127ce6-8d5e-4413-a126-bad1fb280fcc",
