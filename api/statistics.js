9eb7695da2024742ca35dc5e2f0fa59eb54f4745
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 16:20:25 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -1,33 +1,10 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://main.prod.m11g.ajax.systems/statistics/api/last_sync/?entity_types=dev&entity_types=central&entity_types=component", {
-  "headers": {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "uk,en-US;q=0.9,en;q=0.8,ru;q=0.7",
-    "authorization": "",
-    "sec-ch-ua": "\"Chromium\";v=\"148\", \"Google Chrome\";v=\"148\", \"Not/A)Brand\";v=\"99\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "csrftoken": "J2t6jjRNZbA0BSAANwFshop1V3Po1fF2",
-    "access_token" "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NDI5IiwidXNlciI6Ikxpc2l0c3luLlYiLCJleHAiOjE3ODAzMTgxMTcsImlhdCI6MTc4MDMxNDUxNywianRpIjoiNjcxODk5In0.iwh_lSkRCsjhmDWY3xgEWoqUX38osst4dHu-Flixn-lCYAzkPyROMWRvuAjohoKDeoGFHPqV696OW3C2zLgm4CFM7-kMaRIR1m6laCQ2jy3A3n59UFbGaEPgEyiC64gnUtX8K0N-6KBBOSBhxVAwcLHUs7OSpdKqJ57BzcpTZaeZ_xXcBzGQqLlxZxdUE-tXRg8GvVSII5GO6XYSiiDaJHSqwuq2eEetFw2WrHs5J0z-Z_uuhHQN1J_tB97GUqmes4-Tpbg_YUg6xNHEOBFkFqvAp9VtlLjYvg7-wTFutagbi-QNG5eEIjJt0fPL5SvaYlLj_C3kha9qoHxQq4lDzg",
-    "cookie": "cookieyes-consent=consentid:dDVsVFdQZW5vYTV2TXBLYzhXQ3R3QU10cHYyeXNGTWk,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no; csrftoken=J2t6jjRNZbA0BSAANwFshop1V3Po1fF2; access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NDI5IiwidXNlciI6Ikxpc2l0c3luLlYiLCJleHAiOjE3ODAzMTgxMTcsImlhdCI6MTc4MDMxNDUxNywianRpIjoiNjcxODk5In0.iwh_lSkRCsjhmDWY3xgEWoqUX38osst4dHu-Flixn-lCYAzkPyROMWRvuAjohoKDeoGFHPqV696OW3C2zLgm4CFM7-kMaRIR1m6laCQ2jy3A3n59UFbGaEPgEyiC64gnUtX8K0N-6KBBOSBhxVAwcLHUs7OSpdKqJ57BzcpTZaeZ_xXcBzGQqLlxZxdUE-tXRg8GvVSII5GO6XYSiiDaJHSqwuq2eEetFw2WrHs5J0z-Z_uuhHQN1J_tB97GUqmes4-Tpbg_YUg6xNHEOBFkFqvAp9VtlLjYvg7-wTFutagbi-QNG5eEIjJt0fPL5SvaYlLj_C3kha9qoHxQq4lDzg; access_token_exists=true",
-    "Referer": "https://main.prod.m11g.ajax.systems/webaut/statistics/by_product?utm_source=wp&utm_medium=homepage&utm_version=MS4xNC4w&stage=assembling&date_from=2026-05-21&date_to=2026-05-21"
-  },
-  "body": null,
-  "method": "GET"
-});
-
-    if (!response.ok) {
-      return res.status(response.status).json({ error: `Ошибка: ${response.status}` });
-    }
-
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxpvo4tXu7Jn9KDh0cS2GXq7TwdDBZGuYosSzrxnmKR50ZluuO3tnLDYfjB6XAFpAA/exec");
     const data = await response.json();
-    return res.status(200).json(data); // отдаст JSON с постом
+    res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
 
