878dfc7b46525ad231bc4b41057605d75a9f1f69
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 10:55:32 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -1,25 +1,30 @@
 export default async function handler(req, res) {
   try {
-    const targetUrl = "https://main.prod.m11g.ajax.systems/statistics/api/v3/by_grouping_name/?stage=assembling&format=region&location=is76&response_tz=Europe%2FKyiv&time_from=2026-05-30T08%3A00%3A00%2B03%3A00&time_to=2026-05-31T20%3A00%3A00%2B03%3A00&grouping_name0=product_name&grouping_name2=full_name&show_unique=true&unique_by=ftt&register_types=dev&register_types=central&register_types=component&register_types=accessory"; // замени на реальный адрес
+    // URL берём из DevTools → Network → Request URL
+    const targetUrl = "https://site.org/"; // замени на реальный адрес
+
+    // Cookie берём из DevTools → Request Headers → Cookie
+    const cookieHeader = "csrftoken=...; access_token=...; access_token_exists=true";
 
     const response = await fetch(targetUrl, {
       headers: {
-        "Cookie": "csrftoken=J2t6jjRNZbA0BSAANwFshop1V3Po1fF2;access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NDI5IiwidXNlciI6Ikxpc2l0c3luLlYiLCJleHAiOjE3ODAzMDIzMTcsImlhdCI6MTc4MDI5ODcxNywianRpIjoiNjcwNDU5In0.x1KTkxFEgbVFv1H-c7N2cleqXOmGdSqS4bxegV8omSHzn7arb37I-CsU0wRlUiOfj9mfpZcLHumH2TVM6tVs0CgeW0hacgAiBmkEjK-GGzlqSpFyXlizi_WqqmsNuHb6JcnXuaCSNZXgHZQsy7WsCSwp95BbSiZcqzcVx3s8gfBlERrF2LFe61sEELNxzbRmKmuMzuqkbPrqCQVvMhKult4UOAYqooA831iZHEr_GrlFAt8JZE0Jq6Gax7hp-UerLBOVhFp5dnEzi4wTZrqO9fKV9er88-4CvRDfEghoHHFQxtZhWaJvsUhBexs0ut0cmvK2GzM1OBKveGBL_WPN8Q; access_token_exists=true",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
+        "Cookie": cookieHeader,
+        "User-Agent": "Mozilla/5.0"
       }
     });
 
-    // Проверка: достучались ли
+    // Проверяем статус
     if (!response.ok) {
       return res.status(response.status).json({
         error: `Не удалось подключиться. Код ответа: ${response.status}`
       });
     }
 
-    // Просто возвращаем первые 500 символов HTML
-    const html = await response.text();
-    return res.status(200).send(html.slice(0, 500));
+    // Возвращаем первые 300 символов ответа для проверки
+    const text = await response.text();
+    return res.status(200).send(text.slice(0, 300));
 
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
+}
