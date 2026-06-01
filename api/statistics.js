0f7e2245ca5ae1219cb1e13f0c1a7c13e32d971e
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 15:02:05 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -1,7 +1,10 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://fswcopwczgfofuijxcyp.supabase.co/rest/v1/3-Area?select=*", {
-
+  "headers": {
+  
+    "Referer": "https://jh6tpj.csb.app/"
+  },
   "body": null,
   "method": "GET"
 });
