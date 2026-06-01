a1844d8ac4a0c3155f327a4afaf9cd6c6bbdc73a
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-06-01 14:59:38 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -2,12 +2,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://fswcopwczgfofuijxcyp.supabase.co/rest/v1/3-Area?select=*", {
   "headers": {
-    "accept": "*/*",
-    "accept-language": "uk,en-US;q=0.9,en;q=0.8,ru;q=0.7",
-    "accept-profile": "public",
-    "apikey": "sb_publishable_mB28_c5IoxmafFgkXzEORw_w7EUA2MV",
-    
-    "priority": "u=1, i",
+   
     "Referer": "https://jh6tpj.csb.app/"
   },
   "body": null,
