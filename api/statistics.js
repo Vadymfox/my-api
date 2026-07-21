e7d536d59d06b1f7e5772fb8d4bc0db423645614
Vadymfox <113507405+Vadymfox@users.noreply.github.com>
2026-07-21 16:12:52 +0300

    Update statistics.js

--- api/statistics.js
+++ api/statistics.js
@@ -1,18 +1,1 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbw3cHL2sFDypLDaqlFYuuG6yb0pOhq-nTUY1PFSvCaYHCWSOSBigMz0N1Q8wTx4UxQ/exec");
-    const text = await response.text();
-
-    const contentType = response.headers.get("content-type") || "";
-    if (contentType.includes("application/json")) {
-      const data = JSON.parse(text);
-      return res.status(200).json(data);
-    } else {
-      // Отдаём HTML как есть, чтобы браузер показал страницу
-      return res.status(200).send(text);
-    }
-  } catch (error) {
-    return res.status(500).send(`<h1>Ошибка</h1><p>${error.message}</p>`);
-  }
-}
 
